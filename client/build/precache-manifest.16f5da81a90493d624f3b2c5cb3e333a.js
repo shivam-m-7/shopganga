f6eacbd71bb329047be66078a5a037739dd4a51f
shivam-m-7 <shivammmishra7@gmail.com>
2023-02-10 17:58:09 +0530

    4.0

---
+++ client/build/precache-manifest.16f5da81a90493d624f3b2c5cb3e333a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56300c5d346d62a39fb3c3fd1440711",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdb06a490966d3dd688b",
+    "url": "/static/css/2.bb803c14.chunk.css"
+  },
+  {
+    "revision": "152336d0c614d39e8219",
+    "url": "/static/css/main.80be173e.chunk.css"
+  },
+  {
+    "revision": "bdb06a490966d3dd688b",
+    "url": "/static/js/2.0c79ae03.chunk.js"
+  },
+  {
+    "revision": "5795cac972af069c6d9a600320c75127",
+    "url": "/static/js/2.0c79ae03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152336d0c614d39e8219",
+    "url": "/static/js/main.1f4cf676.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "31a737c539bf89313cd1b4d5f43883fd",
+    "url": "/static/media/confirmation.31a737c5.png"
+  }
+]);
